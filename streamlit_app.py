3b6b62ad37213d0096d3935aee96c2ae840b610f
LeeCCooper <110892569+LeeCCooper@users.noreply.github.com>
2022-08-22 14:41:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,11 @@ streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie');
 streamlit.text('🐔 Hard-Boiled Free Range Egg');
 streamlit.text('🥑🍞 Avocado Toast');
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+
+#bring in external CSV file from s3 bucket 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+#set index of my_fruit_list to be the fruit name as opposed to a number 
+my_fruit_list = my_fruit_list.set_index('Fruit');
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
