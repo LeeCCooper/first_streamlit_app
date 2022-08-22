2ace90efbdf10059a6040ef6cdd4229665d4a52b
LeeCCooper <110892569+LeeCCooper@users.noreply.github.com>
2022-08-22 14:28:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,4 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie');
 streamlit.text('🐔 Hard-Boiled Free Range Egg');
 streamlit.text('🥑🍞 Avocado Toast');
--- new header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
