5e81b2a41b4aeb243ccfc034e1dd11ee8919dcaf
LeeCCooper <110892569+LeeCCooper@users.noreply.github.com>
2022-09-27 12:09:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner');
 streamlit.header('Breakfast Menu');
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
@@ -24,7 +27,6 @@ streamlit.dataframe(fruits_to_show);
 #new section to display fruityvice api response 
 streamlit.header('Fruityvice Fruit Advice!');
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi");
 
 #remove the json output 
