9e5415b725646a77310d87a921bae9c63c7e17db
LeeCCooper <110892569+LeeCCooper@users.noreply.github.com>
2022-09-27 12:31:53 +0100

    Update streamlit_app.py
    create a custom function to get more information about the fruit submitted

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,22 @@ streamlit.dataframe(fruits_to_show);
 #new section to display fruityvice api response 
 streamlit.header('Fruityvice Fruit Advice!');
 
+#lets create a repeatable code block called a function
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+  return fruityvice_normalized
+  
+
 #lets build up a try catch block
 try:
   fruit_choice = streamlit.text_input("What fruit would you like information about?")
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+    back_from_function = get_fruityvice_data(fruit_choice)
     #now output to the screen as a table 
-    streamlit.dataframe(fruityvice_normalized);
+    streamlit.dataframe(back_from_function);
 except URLError as e:
   streamlit.error()
   
