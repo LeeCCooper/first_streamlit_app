733c2931cea74831fb9bce42167648559460eee8
LeeCCooper <110892569+LeeCCooper@users.noreply.github.com>
2022-09-27 11:46:25 +0100

    Update streamlit_app.py
    add in additional fruit box to add

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#lets create a fruit to add section 
+streamlit.text("What fruit would you like to add?")
+add_my_fruit = streamlit.text_input("Lee Label", "Cheese Default")
+
 
