b96f1f1565c0520141f0e41275c8d8bb3e016521
LeeCCooper <110892569+LeeCCooper@users.noreply.github.com>
2022-09-27 12:20:57 +0100

    Update streamlit_app.py
    added in try block

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 
 #bring in external CSV file from s3 bucket 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+
 #set index of my_fruit_list to be the fruit name as opposed to a number 
 my_fruit_list = my_fruit_list.set_index('Fruit');
 
@@ -27,17 +28,19 @@ streamlit.dataframe(fruits_to_show);
 #new section to display fruityvice api response 
 streamlit.header('Fruityvice Fruit Advice!');
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi");
-
-#remove the json output 
-#streamlit.text(fruityvice_response.json());
-
-#lets tidyup the json and make it look nice 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
-
-#now output to the screen as a table 
-streamlit.dataframe(fruityvice_normalized);
-
+#lets build up a try catch block
+try:
+  fruit_choice = streamlit.text_input("What fruit would you like information about?")
+  if not fruit_choice
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+    #now output to the screen as a table 
+    streamlit.dataframe(fruityvice_normalized);
+except URLError as e:
+  streamlit.error()
+  
 #dont run anything pas here whilst we troubleshoot
 streamlit.stop()
 
